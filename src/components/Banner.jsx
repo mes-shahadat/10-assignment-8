f9bf090be3f165cacf4f90f7b5f298aa7825ea36
mohammad yasin <binodonprojukti@gmail.com>
2024-11-06 22:31:19 +0600

    banner button now navigates user to dashboard page instead of scrolling to products section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import banner from '../assets/banner.jpg';
 import Products from './Products';
 
@@ -11,7 +12,8 @@ function Banner() {
                 <div className="bg-[#943fdd] text-white rounded-b-xl md:text-center p-4 pb-72 space-y-5 relative">
                     <p className="text-3xl md:text-5xl font-bold max-w-[25ch] mx-auto !leading-tight md:pt-6">Upgrade Your Tech Accessorize with Gadget Heaven Accessories</p>
                     <p className="max-w-[65ch] mx-auto text-gray-100">Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
-                    <a className="bg-white text-[#943fdd] font-semibold px-6 py-3 rounded-full inline-block hover:bg-gray-100" href='#shop'>Shop Now</a>
+                    {/* <a className="bg-white text-[#943fdd] font-semibold px-6 py-3 rounded-full inline-block hover:bg-gray-100" href='#shop'>Shop Now</a> */}
+                    <NavLink className="bg-white text-[#943fdd] font-semibold px-6 py-3 rounded-full inline-block hover:bg-gray-100" to='/dashboard'>Shop Now</NavLink>
 
                     <div className='w-11/12 md:w-8/12 absolute max-sm:bottom-4 left-1/2 transform -translate-x-1/2 p-4 border border-white bg-[#f7f7f780] rounded-3xl'>
                         <img className='rounded-2xl mx-auto xl:h-[500px] w-full object-cover' src={banner} alt="vr headset image" />
